a90a9018bdce09e677fe517fdf8ac247838a3616
CodeWithAnish1 <anishsah683@gmail.com>
2025-08-21 17:38:54 +0530

    added ApiError class for handling API errors

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const ConnectDB= async ()=>{
         console.log(`\n MongoDB Connected !! DB Host: ${ConnectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDb Connection Error",error);
-        process.exit(1)//ye niche ke code ko run hi nhi karne dega matlab terminate kar dega 
+        process.exit(1)//ye isko chalake niche ke code ko run hi nhi karne dega matlab terminate kar dega 
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ dotenv.config({
 })
 
 ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb Connection Failed !!!",err);
+})
